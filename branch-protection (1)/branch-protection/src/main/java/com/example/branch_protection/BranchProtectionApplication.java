971ca68dd6f56c7bcaec281adacff3f89c326ea1
mubu002 <mustafa_buga1@hotmail.com>
2026-02-15 22:32:51 +0100

    Springboot-project added to ide., workflow file created for actions.

---
+++ branch-protection (1)/branch-protection/src/main/java/com/example/branch_protection/BranchProtectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.branch_protection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchProtectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchProtectionApplication.class, args);
+	}
+
+}
---
+++ branch-protection (1)/branch-protection/src/test/java/com/example/branch_protection/BranchProtectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.branch_protection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchProtectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
